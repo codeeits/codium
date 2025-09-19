a2d36b0aeffcc71ed480bfdf0bea4899510e4e68
Andrew-The-Cat <andrew@lekas.tech>
2025-09-19 15:15:05 +0300

    Add database connection and environment variable loading

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"Codium/internal/database"
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type ApiCfg struct {
 	logger log.Logger
+	dbUrl  string
+	db     *database.Queries
 }
 
 /*
@@ -19,37 +26,71 @@ type ApiCfg struct {
 ===========================================
 */
 func main() {
-	cwd, err := os.Getwd()
+	// Initialize logger
+	var cfg *ApiCfg
+	{
+		cwd, err := os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+
+		loggerPath := filepath.Join(cwd, "out", "logs", "api.log")
+		err = os.MkdirAll(filepath.Dir(loggerPath), 0755)
+		if err != nil {
+			panic(err)
+		}
+
+		logFile, err := os.OpenFile(loggerPath, os.O_CREATE|os.O_RDWR, 0666)
+		if err != nil {
+			panic(err)
+		}
+
+		cfg = &ApiCfg{
+			logger: *log.New(logFile, "[API] ", log.LstdFlags),
+		}
+		cfg.logger.Print("Hewwo World! :333")
+	}
+
+	// Load environment variables from .env file
+	err := godotenv.Load()
 	if err != nil {
-		panic(err)
+		cfg.logger.Print("Warning! Error loading .env file- without it you will not be able to connect to the database or any other external services!")
+		cfg.logger.Print("If you run into issues, please make sure you have a .env file in the root directory with the correct variables.")
+
+		cfg.dbUrl = ""
+	} else {
+		cfg.dbUrl = os.Getenv("DB_URL")
 	}
 
-	loggerPath := filepath.Join(cwd, "out", "logs", "api.log")
-	err = os.MkdirAll(filepath.Dir(loggerPath), 0755)
-	if err != nil {
-		panic(err)
+	if cfg.dbUrl != "" {
+		cfg.logger.Print("Using Database URL: " + cfg.dbUrl)
+		db, err := sql.Open("postgres", cfg.dbUrl)
+		if err != nil {
+			cfg.logger.Fatal("Error connecting to the database: ", err)
+		}
+
+		err = db.Ping()
+		if err != nil {
+			cfg.logger.Fatal("Error pinging the database: ", err)
+		}
+
+		cfg.db = database.New(db)
+		cfg.logger.Print("Successfully connected to the database!")
 	}
 
-	logFile, err := os.OpenFile(loggerPath, os.O_CREATE|os.O_RDWR, 0666)
-	if err != nil {
-		panic(err)
-	}
+	// Serve static files from the "App" directory at the "/app/" URL path
+	{
+		mux := http.NewServeMux()
+		mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./App/"))))
 
-	apiCfg := &ApiCfg{
-		logger: *log.New(logFile, "[API] ", log.LstdFlags),
-	}
-	apiCfg.logger.Print("Hewwo World! :333")
+		server := &http.Server{
+			Addr:    ":6767",
+			Handler: mux,
+		}
 
-	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./App/"))))
-
-	server := &http.Server{
-		Addr:    ":6767",
-		Handler: mux,
-	}
-
-	err = server.ListenAndServe()
-	if err != nil {
-		apiCfg.logger.Fatal(err)
+		err = server.ListenAndServe()
+		if err != nil {
+			cfg.logger.Fatal(err)
+		}
 	}
 }
