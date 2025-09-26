09ae33b60c66cc453199eab3d1ac41d463728b4f
Andrew-The-Cat <andrew@lekas.tech>
2025-09-26 11:16:13 +0300

    Finished implementing console mode with admin perms ;>

--- console.go
+++ console.go
@@ -9,17 +9,18 @@ import (
 var commands map[string]func([]string) error
 
 func (cfg *ApiCfg) RegisterCommand(command string, commandFunc func([]string) error) {
-	cfg.logger.Printf("\t|--\tRegistering command %s with function %v", command, commandFunc)
+	cfg.logger.Printf("\t|--\tRegistering command %s", command)
 	commands[command] = commandFunc
 }
 
 func (cfg *ApiCfg) StartConsole() {
 	// Console mode for imputing commands
-	cfg.logger.Print("Starting console")
-	fmt.Println("Starting console")
+	cfg.logger.Print("Starting console...")
+	fmt.Println("Starting console...")
 
 	// Registering commands
 	{
+		cfg.logger.Print("Registering commands")
 		commands = make(map[string]func([]string) error)
 		cfg.RegisterCommand("stop", func(args []string) error {
 			cfg.logger.Print("Stopping application...")
@@ -31,6 +32,13 @@ func (cfg *ApiCfg) StartConsole() {
 			err := cfg.ResetAll()
 			if err != nil {
 				return err
+			}
+			return nil
+		})
+		cfg.RegisterCommand("help", func(args []string) error {
+			fmt.Println("Available commands:")
+			for cmd := range commands {
+				fmt.Println(" -", cmd)
 			}
 			return nil
 		})
