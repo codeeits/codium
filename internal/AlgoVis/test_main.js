3cc97d64f20615e8d19914b02fff0f257a4dd428
Andrew-The-Cat <andrew@lekas.tech>
2026-02-05 18:09:41 +0200

    [FEATURE] Enhance AnimationHandler to support callbacks and add common animation functions for improved visual effects

--- internal/AlgoVis/test_main.js
+++ internal/AlgoVis/test_main.js
@@ -1,18 +1,24 @@
-let view = new Viewport(1220, 1000, 0, 0, "#1B0524", "black", 2);
-let root = new RootContainer(1220, 1000, view);
+let view = new Viewport(1220, 1000, 20, 0, "#1B0524", "black", 2);
+let root = new RootContainer(view.width + 40, view.height, view);
+let vector = [];
+let n = 10
 
-for (let i = 0; i < 10; i++) {
-    let container = new Container(100, 100, i * 110 + 20, root.height / 2 - 50, "rgba(255,255,255,0.1)", "white", 2, "center");
+for (let i = 0; i < n; i++) {
+    let rand = Math.floor(Math.random() * (n - 1)) + 1;
+    let size_y = (rand / n) * (root.height * 3 / 5);
+    let size_X = (root.width / n) * 0.8;
+    let container = new Container(size_X, size_y, i * (root.width / n), root.height / 2 + 100 - size_y, "rgba(255,255,255,0.1)", "white", size_X > 10 ? 2 : 0, "center");
     let element = document.createElement("p");
-    element.innerText = i.toString();
+    element.innerText = rand.toString();
     element.style.color = "white";
     element.style.fontWeight = "bold";
-    element.style.fontSize = "30px";
+    element.style.fontSize = size_X / 2 > 20 ? (size_X / 2).toString() + "px" : "0px";
     element.style.fontFamily = "Arial, sans-serif";
     element.style.verticalAlign = "middle";
 
     container.setElement(element);
     root.addChild(container);
+    vector.push({container: container, value: rand});
 }
 
 console.log(root);
@@ -67,6 +73,15 @@ viewPortElement.addEventListener("pointermove", (e) => {
     scheduleRender();
 });
 
+viewPortElement.addEventListener("pointerover", (e) => {
+    if (dragging) {
+        viewPortElement.style.cursor = "none";
+    }
+    else {
+        viewPortElement.style.cursor = "move";
+    }
+})
+
 function stopDrag(e) {
     if (!dragging) return;
     // release pointer capture
@@ -82,15 +97,41 @@ viewPortElement.addEventListener("pointerup", stopDrag);
 viewPortElement.addEventListener("pointercancel", stopDrag);
 
 let animator = new AnimationHandler()
-animator.ScheduleAnimation((deltaTime) => {
-    root.children[1].rel_x += 3 * deltaTime;
-    scheduleRender();
-    console.log("Animating frame" + deltaTime);
-}, 0, 120);
-animator.ScheduleAnimationAfterPrevious((deltaTime) => {
-    root.children[2].rel_y += 5 * deltaTime;
-    scheduleRender();
-    console.log("Animating frame" + deltaTime);
-}, 120);
+
+function swapBoxes (i, j, durationFrames) {
+    let startX1 = vector[i].container.rel_x;
+    let startX2 = vector[j].container.rel_x;
+    animator.ScheduleAnimationAfterPrevious((deltaTime) => {
+        COMMON_ANIMATIONS.LinearInterpolation(startX1, startX2, (v) => {
+            vector[i].container.rel_x = v;
+            scheduleRender();
+        })(COMMON_ANIMATION_EASING_FUNCTIONS.EaseInOutCubic(deltaTime));
+
+        COMMON_ANIMATIONS.LinearInterpolation(startX2, startX1, (v) => {
+            vector[j].container.rel_x = v;
+            scheduleRender();
+        })(COMMON_ANIMATION_EASING_FUNCTIONS.EaseInOutCubic(deltaTime));
+
+        return deltaTime >= 1.0;
+    }, durationFrames, () => {
+        console.log("Finished swapping", i, j);
+        let temp = vector[i];
+        vector[i] = vector[j];
+        vector[j] = temp;
+    }); // :D :DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDd
+}
+
+let vector_copies = vector.map(v => ({...v})); // create a shallow copy for reference
+console.log(vector.toString())
+for (let i = 0; i < n  - 1; i++) {
+    for (let j = i + 1; j < n; j++) {
+        if (vector_copies[i].value > vector_copies[j].value) {
+            swapBoxes(i, j, 30);
+            let temp = vector_copies[i];
+            vector_copies[i] = vector_copies[j];
+            vector_copies[j] = temp;
+        }
+    }
+}
 
 animator.Start();
