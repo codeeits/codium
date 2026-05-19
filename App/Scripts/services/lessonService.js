9193d8e1514b240518eed3fcf11e102a7688cdcc
codeeits <mail@its-codee.com>
2026-05-19 11:40:46 +0300

    refactor: Simplify deleteAllLessons method by removing redundant file deletion logic

--- App/Scripts/services/lessonService.js
+++ App/Scripts/services/lessonService.js
@@ -25,11 +25,7 @@ export class LessonService {
     async deleteAllLessons() {
         const lessons = await this.getLessons();
         for (const lesson of lessons) {
-            await this.deleteLesson(lesson.lesson.ID);
-            // delete files too
-            if (lesson.lesson.ContentID) {
-                await this.api.fileManager.deleteFile(lesson.lesson.ContentID);
-            }
+            await this.deleteLesson(lesson.lesson.ID);            
         }
     }
 
